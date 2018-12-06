4f8ade7c181d4057aa1dcd9ae7854f4b2a909839
Mathieu <promathieuthiry@gmail.com>
2018-12-06 12:56:51 +0100

    mise a jour newsletter

--- src/components/blog/card.js
+++ src/components/blog/card.js
@@ -2,23 +2,25 @@ import React, { Component } from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import Portraitfemme from '../../assets/img/portraitcrop.jpg';
 import Imagecover1 from '../../assets/img/neonbrand-618322-unsplash.jpg';
+import './card.css';
 
-
-const Card = () => {
+const Card = () => { 
   return (
     <div className="cardglobal">
       <div className="container">
-        <h3 className="center-align">BLOG</h3>
+        <h3 className="center-align titreblog1">BLOG</h3>
         <div className="row">
           <div className="col s12 m6 l4">
             <div className="card">
               <div className="card-image">
-                <img src={Imagecover1} alt="" />
-                <span className="card-title text-darken-5">PEUT-ON FAIRE DE LA RÉDACTION WEB ÉTHIQUE ?</span>
+                <img class="materialboxed" src={Imagecover1} alt="" />
+
+                {/* <span className="card-title text-darken-5 badge white black-text badgereading">5min reading</span> */}
               </div>
               <div className="card-content">
-                <p className="left-align">I am a very simple card. I am good at containing small bits of information.
-                  I am convenient because I require little markup to use effectively.</p>
+                <p className="left-align"><b>PEUT-ON FAIRE DE LA RÉDACTION WEB ÉTHIQUE ?</b></p>
+                <hr />
+                <p className="left-align">Nous, Rédacteurs Web, avons une responsabilité dans la propagation des clichés sociétaux. Nous avons la possibilité d’ouvrir les textes à plus de nuance, ou au contraire de les laisser s’enfermer dans les biais des vieilles publicités.</p>
                 <br />
 
                 <div className="card-action">
@@ -28,8 +30,8 @@ const Card = () => {
                       <img src={Portraitfemme} alt="" className="circle responsive-img" />
                     </div>
                     <div className="col s8">
-                      <span className="black-text">
-                        Anais Jaunai
+                      <span className="black-text left-align">
+                        <b>Anais Jaunai</b>
                       </span>
                     </div>
                   </div>
@@ -43,7 +45,6 @@ const Card = () => {
 
     </div>
   );
-
 };
 
 export default Card;
