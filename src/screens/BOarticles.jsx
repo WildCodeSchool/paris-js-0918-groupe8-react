b7e9cd9f931b82f5e60292d38c3231494c4c6620
GFerry <ferry.g@neuf.fr>
2018-12-06 17:57:43 +0100

    ajout des routes front BO

---
+++ src/screens/BOarticles.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const BOarticles = () => (
+  <div>
+    <p>Articles Back-Office</p>
+  </div>
+);
+
+export default BOarticles;
---
+++ src/screens/BOmedias.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const BOmedias = () => (
+  <div>
+    <p>Médias Back-Office</p>
+  </div>
+);
+
+export default BOmedias;
