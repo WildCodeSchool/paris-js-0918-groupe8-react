79b8d45ccc9f3e69fb6bf95d9596be4931f829bb
Mathieu <promathieuthiry@gmail.com>
2018-12-06 16:30:04 +0100

    insertion service

--- src/components/cover/cover.js
+++ src/components/cover/cover.js
@@ -1,38 +1,32 @@
 import React, { Component } from "react";
-import "materialize-css/dist/css/materialize.min.css";
 import "./cover.css";
 
+const Coverimage = () => {
+    return (
+        <div className="global">
 
+            <div class="container">
+                <p className="titrecovertest">Conteur Digital</p>
 
-class Coverimage extends Component {
+                <div class="row">
 
-
-    render() {
-        return (
-            <div className="global">
-
-                <div class="container">
-                    <p className="titrecovertest">Conteur Digital</p>
-
-                    <div class="row">
-
-                        <p className="enonce">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?
+                    <p className="enonce">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?
                          </p>
 
-                    </div>
-                    <div class="row">
+                </div>
+                <div class="row">
                     <div className="coverbuttonname">
-                    <a  href="#test" className="waves-effect waves-light btn indigo darken-2" >Mathias Savary</a>
-                    <a  href="#test" className="waves-effect waves-light btn red darken-1">Anais Jaunay</a>
+                        <a href="#test" className="waves-effect waves-light btn indigo darken-2" >Mathias Savary</a>
+                        <a href="#test" className="waves-effect waves-light btn red darken-1">Anais Jaunay</a>
 
-                    </div>
-                   
                     </div>
 
                 </div>
+
             </div>
-        );
-    }
+        </div>
+    );
+
 }
 
 export default Coverimage;
--- src/components/newsletter/newsletter.js
+++ src/components/newsletter/newsletter.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import 'materialize-css/dist/css/materialize.min.css';
 import './newsletter.css';
 
 
@@ -9,44 +8,46 @@ const Newsletter = () => {
       <div className="container">
         <h3 className="center-align titreblog1">Newsletter</h3>
 
-        <div className="z-depth-3">
-          <div className="row">
-            <div className="col s12 m6">
-              <p className="titledownloadlivretblanc">Télécharger le livre blanc</p>
-              <hr />
-              <p className="left-align">Pour recevoir le livre blanc, il suffit de vous inscrire à notre newsletter</p>
-              <p className="left-align">
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehender
-              </p>
+        <div className="row newsletterblock grey lighten-5">
+          <div className="col s12 m6">
+            <p className="titledownloadlivretblanc">Télécharger le livre blanc</p>
+            <hr />
+            <p className="left-align">Pour recevoir le livre blanc, il suffit de vous inscrire à notre newsletter</p>
+            <p className="left-align">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+              ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehender
+            </p>
+          </div>
+
+          <form className="col s12 m4 offset-m2">
+            <div className="row">
+              <div className="input-field col s12">
+                <input id="first_name" type="text" className="validate" />
+                <label for="first_name">Prénom</label>
+              </div>
             </div>
 
-            <form className="col s12 m4 offset-m2">
-              <div className="row">
-                <div className="input-field col s10">
-                  <input id="first_name" type="text" className="validate" />
-                  <label for="first_name">Prénom</label>
-                </div>
+            <div class="row">
+              <div class="input-field col s12">
+                <input id="email" type="email" className="validate" />
+                <label for="email">Email</label>
               </div>
+            </div>
+            <div className="row">
+              <div className="input-field col s12">
+                <button className="btn waves-effect waves-light blue darken-1" type="submit" name="action">Envoyer
+                  <i className="material-icons right">
+                  send
+                  </i>
+                </button>
+              </div>
+            </div>
 
-              <div class="row">
-                <div class="input-field col s10">
-                  <input id="email" type="email" className="validate" />
-                  <label for="email">Email</label>
-                </div>
-              </div>
-              <div className="row">
-                <div className="input-field col s10">
-                  <button className="btn waves-effect waves-light blue darken-1" type="submit" name="action">Envoyer
-                    <i className="material-icons right">send</i>
-                  </button>
-                </div>
-              </div>
-
-            </form>
-          </div>
+          </form>
         </div>
       </div>
     </div>
+
   );
 };
 
