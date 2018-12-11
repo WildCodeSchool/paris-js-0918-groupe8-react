39ff4dba2031b0d3874cca2be049f79ef2a288d3
GFerry <ferry.g@neuf.fr>
2018-12-11 10:19:11 +0100

    modif couleur txt input

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -3,35 +3,33 @@ import './contact.css';
 
 
 const Contact = () => (
-  <div className="newsletterglobal" id="contact">
+  <div className="contactglobal" id="contact">
     <div className="container">
       <h3 className="center-align titreblog1">CONTACT</h3>
 
       <div className="row grey lighten-5">
 
-
         <form className="col s12 m6 offset-m6">
 
           <div className="input-field left-align col s12">
-            <input id="first_namee" type="text" className="validate" />
-            <label HtmlFor="first_namee">Prénom*</label>
+            <input id="first_namee" type="text" className="validate contact-input" />
+            <label htmlFor="first_namee">Prénom*</label>
           </div>
           <div className="input-field col s12 left-align">
-            <input id="last_name" type="text" className="validate" />
-            <label HtmlFor="last_name">Nom*</label>
+            <input id="last_name" type="text" className="validate contact-input" />
+            <label htmlFor="last_name">Nom*</label>
+          </div>
+          <div className="input-field col s12">
+            <input id="mailcontact" type="email" className="validate contact-input" />
+            <label htmlfor="mailcontact">Email*</label>
+          </div>
+          <div className="input-field col s12">
+            <input id="entreprise" type="text" className="validate contact-input" />
+            <label htmlFor="entreprise">Entreprise</label>
           </div>
 
           <div className="input-field col s12">
-            <input id="mailcontact" type="email" className="validate" />
-            <label Htmlfor="mailcontact">Email*</label>
-          </div>
-          <div className="input-field col s12">
-            <input id="entreprise" type="text" className="validate" />
-            <label HtmlFor="entreprise">Entreprise</label>
-          </div>
-
-          <div className="input-field col s12">
-            <textarea id="textarea2" className="materialize-textarea" data-length="120" />
+            <textarea id="textarea2" className="materialize-textarea contact-input" data-length="120" />
             <label for="textarea2">Textarea</label>
           </div>
           <div className="row">
@@ -46,14 +44,14 @@ const Contact = () => (
             </div>
 
             <div className="input-field col s5 offset-s1">
-              <button className="btn waves-effect waves-light blue darken-1" type="submit" name="action">Envoyer
+              <button className="btn waves-effect waves-light blue darken-1" type="submit" name="action">
+              Envoyer
                 <i className="material-icons right">
                 send
                 </i>
               </button>
             </div>
           </div>
-
         </form>
       </div>
     </div>
