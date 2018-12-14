1f60a80a5717142251224392b9e2611905eff59f
Af1ne <delphine.brunet@gmail.com>
2018-12-14 11:38:08 +0100

    fix console err + adding GetArticles copomsant + adding axios module

--- src/screens/BOarticles.jsx
+++ src/screens/BOarticles.jsx
@@ -1,24 +1,12 @@
 import React from 'react';
 import BofficeMenu from '../components/boMenu/BofficeMenu';
+import GetArticles from '../components/boarticle/GetArticles';
 import BoFooter from '../components/bofooter/BoFooter';
 
 const BOarticles = () => (
   <div>
     <BofficeMenu />
-
-    <div>
-      <p className="measure center">
-        <form className="pa4 black-40">
-          <label htmlFor="caseArticle" className="f1 measure ">
-          Ecrire un article
-            {' '}
-            <textarea id="caseArticle" className=" h5 w-100 " />
-          </label>
-
-        </form>
-        <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Valider" />
-      </p>
-    </div>
+    <GetArticles />
     <BoFooter />
   </div>
 );
