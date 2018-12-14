ddb8fecc19acbb37674f2a7256b3971b35b42ad4
Mathieu <promathieuthiry@gmail.com>
2018-12-14 12:36:46 +0100

    test

--- src/components/cover/cover.jsx
+++ src/components/cover/cover.jsx
@@ -1,32 +1,34 @@
-import React from "react";
-import "./cover.css";
+import React from 'react';
+import './cover.css';
 
-const Coverimage = () => {
-    return (
-        <div className="global" id="home">
+const Coverimage = () => (
 
-            <div class="container">
-                <p className="titrecovertest center-align">Conteur Digital</p>
+  <div className="global" id="home">
 
-                <div class="row">
+    <div className="container">
+      <p className="titrecovertest center-align">Conteur Digital</p>
 
-                    <p className="enonce">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?
-                         </p>
+      <div className="row">
 
-                </div>
-                <div class="row">
-                    <div className="coverbuttonname">
-                        <a href="#test" className="waves-effect waves-light btn indigo darken-2" >Mathias Savary</a>
-                        <a href="#test" className="waves-effect waves-light btn red darken-1">Anais Jaunay</a>
+        <p className="enonce">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit
+        optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates
+        voluptas?
+        </p>
+      </div>
+      <div className="row">
+        <div className="coverbuttonname">
+          <a href="#test" className="waves-effect waves-light btn indigo darken-2">Mathias Savary</a>
+          <a href="#test" className="waves-effect waves-light btn red darken-1">Anais Jaunay</a>
 
-                    </div>
-
-                </div>
-
-            </div>
         </div>
-    );
 
-}
+      </div>
+
+    </div>
+  </div>
+
+);
 
 export default Coverimage;
