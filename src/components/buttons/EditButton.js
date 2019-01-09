bb04573934e5105d3357700a3a274fe0f780a120
julielm77 <julielyminh@gmail.com>
2019-01-09 17:50:51 +0100

    tinymce axios content

--- src/components/buttons/EditButton.js
+++ src/components/buttons/EditButton.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Axios from 'axios';
 
-const EditButton = () => (
-  <button type="submit">
-    <i className="material-icons">
-      create
-    </i>
-  </button>
-);
+class EditButton extends Component {
+  setEdit = () => {
+    Axios.put(
+      `/api/articles/blog/${this.props.id_article}`,
+      { front_page_favorite: !this.props.active },
+    )
+      .then(this.props.loadData);
+  }
+
+
+  render() {
+    return (
+      <div>
+        <button type="submit" onClick={this.setEdit}>
+          {this.props.active ? <i className="material-icons md-yellow">create</i> : (<i className="material-icons">create</i>)}
+        </button>
+      </div>
+    );
+  }
+}
+
+EditButton.propTypes = {
+  active: PropTypes.number.isRequired,
+};
 
 export default EditButton;
--- src/components/buttons/StarButton.js
+++ src/components/buttons/StarButton.js
@@ -10,6 +10,7 @@ class StarButton extends Component {
   setFavorite = () => {
     const { id_article, active, loadData } = this.props;
     Axios.put(
+<<<<<<< HEAD
       `/api/articles/blog/${id_article}`,
       { front_page_favorite: !active }
     )
@@ -19,12 +20,21 @@ class StarButton extends Component {
         }
       })
       .then(loadData);
+=======
+      `/api/articles/blog/${this.props.id_article}`,
+      { front_page_favorite: !this.props.active },
+    )
+      .then(this.props.loadData);
+>>>>>>> tinymce axios content
   }
 
 
   render() {
+<<<<<<< HEAD
     const { active } = this.props;
 
+=======
+>>>>>>> tinymce axios content
     return (
       <div>
         <button type="submit" onClick={this.setFavorite}>
