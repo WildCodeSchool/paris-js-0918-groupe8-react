f8c46be4e4bdd6b81c189df438ae498e082e859e
Monica TARABUSI <code.mta@gmail.com>
2019-01-09 19:44:33 +0100

    components-screens-BOarticles.js : suppression BoFooter

--- src/screens/BOarticles.jsx
+++ src/screens/BOarticles.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import BofficeMenu from '../components/boMenu/BofficeMenu';
 import GetArticles from '../components/boarticle/GetArticles';
-import BoFooter from '../components/bofooter/BoFooter';
 
 const BOarticles = () => (
   <div>
     <BofficeMenu />
     <GetArticles />
-    <BoFooter />
   </div>
 );
 
