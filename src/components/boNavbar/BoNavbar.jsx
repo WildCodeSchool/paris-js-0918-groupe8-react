f1dc0d35812644ae0e1a6fac5373c032a26382d1
Monica TARABUSI <code.mta@gmail.com>
2019-01-10 19:38:40 +0100

    resolution bug burger sidenav components/boNavbar/BoNavbar

--- src/components/boNavbar/BoNavbar.jsx
+++ src/components/boNavbar/BoNavbar.jsx
@@ -1,30 +1,45 @@
 /* global document */
-import React from 'react';
+import React, { Component } from 'react';
 // import { NavLink } from 'react-router-dom';
 import { NavHashLink as NavLink } from 'react-router-hash-link';
+import M from 'materialize-css/dist/js/materialize.min';
 
 import routes from '../../routes/routes';
 
-const BoNavbar = () => (
-  <div>
-    <ul id="slide-out" className="sidenav sidenav-fixed">
-      {routes.filter(elem => elem.path.includes('/admin'))
-        .map(prop => (
-          <li>
-            <NavLink
-              exact
-              to={prop.path}
-              key={prop.path}
-            >
-              {prop.name}
-            </NavLink>
-          </li>
-        ))}
-    </ul>
-    <a href="#test" data-target="slide-out" className="sidenav-trigger">
-      <i className="material-icons">menu</i>
-    </a>
-  </div>
-);
+class BoNavbar extends Component {
+  componentDidMount() {
+    const elem = document.querySelector('.sidenav');
+    M.Sidenav.init(elem, {
+      edge: 'left',
+      inDuration: 250,
+    });
+  }
+
+  render() {
+    return (
+
+      <div>
+        <div>
+          <ul id="slide-out" className="sidenav sidenav-fixed">
+            {routes.filter(elem => elem.path.includes('/admin')).map(prop => (
+              <li>
+                <NavLink
+                  exact
+                  to={prop.path}
+                  key={prop.path}
+                >
+                  {prop.name}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+          <a href="#test" data-target="slide-out" className="sidenav-trigger">
+            <i className="material-icons">menu</i>
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default BoNavbar;
