177f5d03ef3dae94c16255b0d4d72529f5b7899f
Af1ne <delphine.brunet@gmail.com>
2019-01-11 17:25:03 +0100

    repare BlogList

---
+++ src/screens/BlogList.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from '../components/navbar/navbar';
+import Blog from '../components/blog/card';
+import Footer from '../components/footer/footer';
+
+
+const BlogList = () => (
+
+
+  <div>
+    <Navbar />
+    <Blog />
+    <Footer />
+  </div>
+);
+
+export default BlogList;
