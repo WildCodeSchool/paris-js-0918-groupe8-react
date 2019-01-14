7f59d3b53919ed40c7e10a18becaa763ecc1b0f2
Af1ne <delphine.brunet@gmail.com>
2019-01-14 17:14:54 +0100

    article navbar fonctionnal(publish / brouillon / supprimé)

--- src/components/boarticle/BOArticleNav.js
+++ src/components/boarticle/BOArticleNav.js
@@ -1,20 +1,125 @@
 import React, { Component } from 'react';
-
-// import './BOArticleNav.css';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import BOFilterArticle from './BoFilterArticle';
 
 class BOArticleNav extends Component {
+  state = {
+    articles: [],
+    status: ['published', 'writting_progress', 'archived'],
+    nbAll: 0,
+    nbPublished: 0,
+    nbWritting_progress: 0,
+    nbArchived: 0,
+  }
+
+  componentDidMount() {
+    this.loadData();
+  }
+
+  loadData = async () => {
+    const response = await axios.get('/api/articles/blog');
+    this.setState({
+      articles: response.data,
+      nbAll: response.data.length,
+      nbPublished: response.data.filter(e => e.blog_status.includes('published')).length,
+      nbWritting_progress: response.data.filter(e => e.blog_status.includes('writting_progress')).length,
+      nbArchived: response.data.filter(e => e.blog_status.includes('archived')).length,
+    });
+    // console.log(this.state.articles.map.filter(e => status.includes('published'))
+  }
+
+  setStatus = (e) => {
+    switch (e.target.id) {
+      case '#tous':
+        this.setState({
+          status: ['published', 'writting_progress', 'archived'],
+        });
+        break;
+      case '#publies':
+        this.setState({
+          status: ['published'],
+        });
+        break;
+      case '#brouillons':
+        this.setState({
+          status: ['writting_progress'],
+          // nbWritting_progress: ,
+        });
+        break;
+      case '#supprimes':
+        this.setState({
+          status: ['archived'],
+          // nbArchived: 0,
+        });
+        break;
+      default:
+        this.setState({
+          status: ['published', 'writting_progress', 'archived'],
+        });
+        break;
+    }
+  }
 
   render() {
+    const { articles, status, nbAll, nbPublished, nbWritting_progress, nbArchived } = this.state;
 
     return (
-      <nav className="nav" style={{ background: '#003F5F', color: '#FFFFFF' }}>
-        <ul className="tabs tabs-transparent ">
-          <li className="tab waves-effect waves-light"><a href="#tous">Tous <span className="badge">x</span></a></li>
-          <li className="tab waves-effect waves-light"><a href="#publies">Publiés <span className="badge">x</span></a></li>
-          <li className="tab waves-effect waves-light"><a href="#brouillon">Brouillon <span className="badge">x</span></a></li>
-          <li className="tab waves-effect waves-light"><a href="#supprime">Supprimé <span className="badge">x</span></a></li>
-        </ul>
-      </nav>
+      <div>
+        <nav className="nav" style={{ background: '#003F5F', color: '#FFFFFF' }}>
+          <ul className="tabs tabs-transparent">
+            <li className="tab waves-effect waves-light">
+              <Link
+                to="#tous"
+                onClick={this.setStatus}
+                id="#tous"
+                activeclassname="active"
+                activestyle={{ fontWeight: 'Bold' }}
+              >
+                Tous
+                <span className="badge">{nbAll}</span>
+              </Link>
+            </li>
+            <li className="tab waves-effect waves-light">
+              <Link
+                to="#publies"
+                onClick={this.setStatus}
+                id="#publies"
+                activeclassname="active"
+                activestyle={{ fontWeight: 'Bold' }}
+              >
+                Publiés
+                <span className="badge">{nbPublished}</span>
+              </Link>
+            </li>
+            <li className="tab waves-effect waves-light">
+              <Link
+                to="#brouillons"
+                onClick={this.setStatus}
+                id="#brouillons"
+                activeclassname="active"
+                activestyle={{ fontWeight: 'Bold' }}
+              >
+                Brouillons
+                <span className="badge">{nbWritting_progress}</span>
+              </Link>
+            </li>
+            <li className="tab waves-effect waves-light">
+              <Link
+                to="#supprimes"
+                onClick={this.setStatus}
+                id="#supprimes"
+                activeclassname="active"
+                activestyle={{ fontWeight: 'Bold' }}
+              >
+                Supprimés
+                <span className="badge">{nbArchived}</span>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <BOFilterArticle articles={articles} status={status} loadData={this.loadData} />
+      </div>
     );
   }
 }
