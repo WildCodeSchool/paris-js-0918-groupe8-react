5ba2a77fcb7e36c240709e916a9dc54419b56ee7
Monica TARABUSI <code.mta@gmail.com>
2019-01-16 00:23:27 +0100

    components/newsletter/newsletter : relié à la BD, mais attention, seulement après pull-request node (short_content - ménage <div> et <label>, à valider avec Mathieu

--- src/components/newsletter/newsletter.jsx
+++ src/components/newsletter/newsletter.jsx
@@ -1,52 +1,72 @@
-import React from 'react';
+import React, { Component} from 'react';
+import axios from 'axios';
+
 import './newsletter.css';
 
 
-const Newsletter = () => (
-  <div className="newsletterglobal">
-    <div className="container">
-      <h3 className="center-align titreblog1">NEWSLETTER</h3>
+class Newsletter extends Component {
+  state = {
+    newsletterTable: [],
+  }
 
-      <div className="row newsletterblock grey lighten-5">
-        <div className="col s12 m6">
-          <p className="titledownloadlivretblanc">Devenez conteur digital :</p>
-          <hr />
-          <p className="left-align">Chaque semaine pendant 3 prochains mois nous vous envoyons une technique de storytelling simple et applicable pour accroître l’impact de votre communication digitale.</p>
-          <p className="left-align">
-          En cadeau, recevez 10 conseils des plus grands storytellers et comment les utiliser pour votre entreprise.
+  // APPEL DE LA REQUETE
+  componentDidMount() {
+    this.getNewsletterTable();
+  }
 
-          </p>
-        </div>
+  // REQUETE DE L'ARTICLE 4 - NEWSLETTER
+  getNewsletterTable = async () => {
+    const res = await axios.get('/api/articles/newsletter');
+    this.setState({ newsletterTable: res.data[0] });
+  }
 
-        <form className="col s12 m4 offset-m2">
-          <div className="row">
-            <div className="input-field col s12">
+  render() {
+    // déconstruction du state et alias du this.state
+    const { newsletterTable } = this.state;
+
+    return (
+      <div className="newsletterglobal container">
+
+        <h3 className="center-align titreblog1">NEWSLETTER</h3>
+
+        <div className="row newsletterblock grey lighten-5">
+          <div className="col s12 m6">
+            <p className="titledownloadlivretblanc">
+              {newsletterTable.title}
+            </p>
+            <hr />
+            <p className="left-align">
+              {newsletterTable.short_content}
+            </p>
+          </div>
+
+
+          <form className="col s12 m4 offset-m2">
+
+            <label className="row input-field col s12 active" htmlFor="first_name2">
+              Prénom
               <input value="" id="first_name2" type="text" className="validate" />
-              <label className="active" htmlFor="first_name2">Prénom</label>
-            </div>
-          </div>
-          <div className="row">
-            <div className="input-field col s12">
+            </label>
+
+            <label className="input-field col s12" htmlFor="row email">
+              Email
               <input id="email" type="email" className="validate newsletter-input" />
-              <label htmlFor="email">Email</label>
-            </div>
-          </div>
-          <div className="row">
-            <div className="input-field col s12 boutonenvoienewsletter">
+            </label>
+
+            <div className="row input-field col s12 boutonenvoienewsletter">
               <button className="btn waves-effect waves-light blue darken-1" type="submit" name="action">
               Envoyer
                 <i className="material-icons right">
-                send
+                  send
                 </i>
               </button>
             </div>
-          </div>
 
-        </form>
+          </form>
+        </div>
       </div>
-    </div>
-  </div>
-
-);
+    );
+  }
+}
 
 export default Newsletter;
