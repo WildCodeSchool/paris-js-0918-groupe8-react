25ba16c44a479a5ab9a00cc54f0985d7e7cd1dd2
Monica TARABUSI <code.mta@gmail.com>
2019-01-15 20:13:14 +0100

    components/charte.charte : reliés à la BD - réduction nombre div, en cumulant les classNames - <p> vide pour garder la mise en page, en attente de correction

--- src/components/charte/charte.jsx
+++ src/components/charte/charte.jsx
@@ -1,27 +1,41 @@
-import React from 'react';
-import './charte.css';
+import React, { Component } from 'react';
+import axios from 'axios';
 import { NavHashLink as NavLink } from 'react-router-hash-link';
 
-const Charte = () => (
-  <div className="cardglobal">
+import './charte.css';
 
-    <div className="testimage">
-      <div className="textecharte">
-        <div className="container">
-          <h3 className="center-align titrecharte1 white-text">CHARTE</h3>
-          <p className="center-align white-text phrasenumber1">Internet n’est pas le royaume des développeurs, des ingénieurs, des data scientistes ou des entrepreneurs. Ils ne font qu’en définir les contours. Ils construisent la carcasse.</p>
-          <p className="center-align white-text phrasenumber2">On vous a menti</p>
-          <div className="bouttonensavoirplus">
-            <NavLink exact smooth to='/charte' className="center-align waves-effect waves-light btn deep-purple accent-3"> En savoir plus</NavLink>
-          </div>
-          <p className="center-align white-text phrasenumber3">"Content is king"</p>
+class Charte extends Component {
+  state = {
+    charteTable: {}
+  }
 
+  // APPEL DE LA REQUETE
+  componentDidMount() {
+    this.getCharteTable();
+  }
+
+  // REQUETE DE L'ARTICLE 2 = CHARTE-SHORT
+  getCharteTable = async () => {
+    const response = await axios.get('/api/articles/charte-short');
+    this.setState({ charteTable: response.data[0] });
+  }
+
+  render() {
+    const { charteTable } = this.state;
+
+    return (
+      <div className="cardglobal testimage textecharte container">
+        <h3 className="center-align titrecharte1 white-text">{charteTable.title}</h3>
+        <p className="center-align white-text phrasenumber1">{charteTable.short_content}</p>
+        <div className="bouttonensavoirplus">
+          <NavLink exact smooth to="/charte" className="center-align waves-effect waves-light btn deep-purple accent-3"> En savoir plus</NavLink>
         </div>
+
+        {/* il faut laisser le <p> suivant, vide, tant que la mise en page n'est pas modifiée */}
+        <p className="center-align white-text phrasenumber3"> </p>
       </div>
-
-    </div>
-
-  </div>
-);
+    );
+  }
+}
 
 export default Charte;
