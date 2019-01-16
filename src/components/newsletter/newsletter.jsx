245d8dc0aea4251051ddda83f4d9fa5d15c85f84
Monica TARABUSI <code.mta@gmail.com>
2019-01-16 10:15:15 +0100

    components/newsletter/newsletter : remise en état de l'<input> pas à l'intérieur du <label>. Eslint voit rouge, mais conforme à Materialize et redonne le mouvement dans le formulaire

--- src/components/newsletter/newsletter.jsx
+++ src/components/newsletter/newsletter.jsx
@@ -25,44 +25,50 @@ class Newsletter extends Component {
     const { newsletterTable } = this.state;
 
     return (
-      <div className="newsletterglobal container">
+      <div className="newsletterglobal">
 
-        <h3 className="center-align titreblog1">NEWSLETTER</h3>
+        <div className="container">
+          <h3 className="center-align titreblog1">NEWSLETTER</h3>
 
-        <div className="row newsletterblock grey lighten-5">
-          <div className="col s12 m6">
-            <p className="titledownloadlivretblanc">
-              {newsletterTable.title}
-            </p>
-            <hr />
-            <p className="left-align">
-              {newsletterTable.short_content}
-            </p>
-          </div>
-
-
-          <form className="col s12 m4 offset-m2">
-
-            <label className="row input-field col s12 active" htmlFor="first_name2">
-              Prénom
-              <input value="" id="first_name2" type="text" className="validate" />
-            </label>
-
-            <label className="input-field col s12" htmlFor="row email">
-              Email
-              <input id="email" type="email" className="validate newsletter-input" />
-            </label>
-
-            <div className="row input-field col s12 boutonenvoienewsletter">
-              <button className="btn waves-effect waves-light blue darken-1" type="submit" name="action">
-              Envoyer
-                <i className="material-icons right">
-                  send
-                </i>
-              </button>
+          <div className="row newsletterblock grey lighten-5">
+            <div className="col s12 m6">
+              <p className="titledownloadlivretblanc">
+                {newsletterTable.title}
+              </p>
+              <hr />
+              <p className="left-align">
+                {newsletterTable.short_content}
+              </p>
+              <br />
             </div>
 
-          </form>
+            <form className="col s12 m4 offset-m2">
+              <div className="row">
+                <div className="input-field col s12">
+                  <input value="" id="first_name2" type="text" className="validate" />
+                  <label className="active" htmlFor="first_name2">Prénom</label>
+                </div>
+              </div>
+              <div className="row">
+                <div className="input-field col s12">
+                  <input id="email" type="email" className="validate newsletter-input" />
+                  <label htmlFor="email">Email</label>
+                </div>
+              </div>
+              
+              <div className="row">
+                <div className="input-field col s12 boutonenvoienewsletter">
+                  <button className="btn waves-effect waves-light blue darken-1" type="submit" name="action">
+                    Envoyer
+                    <i className="material-icons right">
+                      send
+                    </i>
+                  </button>
+                </div>
+              </div>
+
+            </form>
+          </div>
         </div>
       </div>
     );
