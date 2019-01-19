e99fc3c27d50e022c522789784f677d4aee8e5b3
Af1ne <delphine.brunet@gmail.com>
2019-01-19 19:43:39 +0100

    adding newletter edit + adding contentnewsletter in front

--- src/components/boEditionAcceuil/BoEditGlobal.js
+++ src/components/boEditionAcceuil/BoEditGlobal.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 import BoCoverEdit from './BoCoverEdit';
 import Coverimage from '../cover/cover';
+
 import BoCharteEdit from './BoCharteEdit';
 import Charte from '../charte/charte';
+
+import BoNewsletterEdit from './BoNewsletterEdit';
 import Newsletter from '../newsletter/newsletter';
 import Service from '../Service/service';
 import About from '../about/about';
@@ -41,10 +44,11 @@ const BoEditGlobal = () => (
     <div className="divider" />
     <div className="section">
       <div className="row">
-        <div className="col s5" />
-        <div className="card-panel teal lighten-5">
-          {/* Inseré le composant ici */}
-        </div>
+        <div className="col s5">
+					<div className="card-panel teal lighten-5">
+						<BoNewsletterEdit />
+					</div>
+				</div>
         <div className="col s7">
           <Newsletter />
         </div>
@@ -54,10 +58,11 @@ const BoEditGlobal = () => (
     <div className="divider" />
     <div className="section">
       <div className="row">
-        <div className="col s5" />
-        <div className="card-panel teal lighten-5">
-          {/* Inseré le composant ici */}
-        </div>
+        <div className="col s5">
+					<div className="card-panel teal lighten-5">
+						{/* Inseré le composant ici */}
+					</div>
+				</div>
         <div className="col s7">
           <Service />
         </div>
@@ -67,10 +72,11 @@ const BoEditGlobal = () => (
     <div className="divider" />
     <div className="section">
       <div className="row">
-        <div className="col s5" />
-        <div className="card-panel teal lighten-5">
-          {/* Inseré le composant ici */}
-        </div>
+        <div className="col s5">
+					<div className="card-panel teal lighten-5">
+						{/* Inseré le composant ici */}
+					</div>
+				</div>
         <div className="col s7">
           <About />
         </div>
