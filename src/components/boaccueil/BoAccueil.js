ffd39605caa51a415e96bd4862258d1702146166
julielm77 <julielyminh@gmail.com>
2019-01-21 12:08:33 +0100

    modif page accueil

---
+++ src/components/boaccueil/BoAccueil.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+const BoAccueil = () => (
+
+  <div className="row">
+    <div className="col s3" />
+    <p className="red lighten-1" style={{ fontSize: '50px' }}>
+      Bienvenue sur le Tableau de Bord
+    </p>
+  </div>
+);
+
+
+export default BoAccueil;
