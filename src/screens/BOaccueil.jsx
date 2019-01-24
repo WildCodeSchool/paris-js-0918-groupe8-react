54744c9a9215553908192fb94e1c0a5a64a696b6
Mathieu <promathieuthiry@gmail.com>
2019-01-24 14:01:13 +0100

    boutton login

--- src/screens/BOaccueil.jsx
+++ src/screens/BOaccueil.jsx
@@ -3,13 +3,16 @@ import React from 'react';
 import GoToSite from '../components/buttons/GoToSite';
 import BoNavbar from '../components/boNavbar/BoNavbar';
 import BoAccueil from '../components/boaccueil/BoAccueil';
+import LogOut from '../components/buttons/Logout';
 
 import './BOgrids.css';
+
 
 const BOaccueil = () => (
   <div className="grid-container">
     <header className="grid-item">
       <GoToSite />
+      <LogOut />
     </header>
     <aside className="grid-item BoNavbar">
       <BoNavbar />
--- src/screens/BOarticles.jsx
+++ src/screens/BOarticles.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import GoToSite from '../components/buttons/GoToSite';
 import BoNavbar from '../components/boNavbar/BoNavbar';
 import BOArticleNav from '../components/boarticle/BOArticleNav';
+import LogOut from '../components/buttons/Logout';
 
 import './BOgrids.css';
 
@@ -10,6 +11,8 @@ const BOarticles = () => (
   <div className="grid-container">
     <header className="grid-item">
       <GoToSite />
+      
+      <LogOut />
     </header>
     <aside className="grid-item BoNavbar">
       <BoNavbar />
