6bca8afff4cf48449c7aa5d83976643b5e6671b1
Mathieu <promathieuthiry@gmail.com>
2019-01-24 15:55:59 +0100

    Correction Button

--- src/screens/BOarticles.jsx
+++ src/screens/BOarticles.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import GoToSite from '../components/buttons/GoToSite';
 import BoNavbar from '../components/boNavbar/BoNavbar';
 import BOArticleNav from '../components/boarticle/BOArticleNav';
-import LogOut from '../components/buttons/Logout';
 
 import './BOgrids.css';
 
@@ -11,8 +10,6 @@ const BOarticles = () => (
   <div className="grid-container">
     <header className="grid-item">
       <GoToSite />
-      
-      <LogOut />
     </header>
     <aside className="grid-item BoNavbar">
       <BoNavbar />
