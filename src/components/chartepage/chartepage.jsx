b8ad7e5f1f0d3d4b10ef695281bd828e1cf0169f
Monica TARABUSI <code.mta@gmail.com>
2019-01-24 19:12:13 +0100

    boarticle/setArticle : checkbox author choice

--- src/components/chartepage/chartepage.jsx
+++ src/components/chartepage/chartepage.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import renderHTML from 'react-render-html';
-import { NavHashLink as NavLink } from 'react-router-hash-link';
 
 import './chartepage.css';
 
